44b3b0044cb94981f7ee0773bd9c12e438c63206
Leonard Farparan <aguinaldo.farparan@gmail.com>
2024-06-06 14:53:59 +0800

    Added navigation and greetings part in homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
 import Homepage from "./Homepage"
 import { useState, createContext } from "react"
+import snowflake from './Homepage/assets/snowflake.svg'
+import fire from './Homepage/assets/fire.svg'
 
 export const TempContext = createContext();
 
 function App() {
   const [temperature, setTemperature] = useState('cold')
+  const [widget, setWidget] = useState(snowflake);
+
+  function themeChange(){
+    setTemperature(temperature === 'cold'? 'warm': 'cold')
+    setWidget( widget === snowflake? fire : snowflake )    
+  }
 
   return (
     <div className={temperature}>
+      <div className="themecon" onClick={themeChange}>
+        <img src={widget} alt="theme-button" className="theme"/>
+      </div>
       <TempContext.Provider value={{ temperature, setTemperature }}>
         <Homepage/>
+        <Homepage/>
       </TempContext.Provider>
+      
     </div>
   )
 }
--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,14 +1,9 @@
 import React, { useState, useContext } from 'react'
 import './Homepage/style/homepage.css'
-import snowflake from './Homepage/assets/snowflake.svg'
-import fire from './Homepage/assets/fire.svg'
 import { TempContext } from './App'
 
 export default function Homepage() {
-  const [widget, setWidget] = useState(snowflake);
   const { temperature, setTemperature } = useContext(TempContext)
-
-  
 
   function themeChange(){
     setTemperature(temperature === 'cold'? 'warm': 'cold')
@@ -18,9 +13,23 @@ export default function Homepage() {
 
   return (
     <div className='header'>
-        <div className="themecon" onClick={themeChange}>
-            <img src={widget} alt="theme-button" className="theme"/>
+      <div className='navigation'>
+        <p className="navigate">PROJECTS</p>
+        <p className="navigate">ABOUT ME</p>
+      </div>
+      <div className="homecon">
+        <div className="greetbox">
+          <h1 className='hello'>Hello, my name is</h1>
+          <h1 className='name'>LEONARD FARPARAN</h1>
+          <p className="greetdescription">
+            I'm an aspiring Web developer who specializes on Javascript
+            libraries & frameworks and is willing to learn new technologies
+          </p>
         </div>
+        <div className="blobbox">
+
+        </div>
+      </div>
     </div>
   )
 }
