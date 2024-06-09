d67cd1b791ad767fe799870cbc1d41bb57601e13
Leonard Farparan <aguinaldo.farparan@gmail.com>
2024-06-09 20:04:10 +0800

    Added description on the first project

--- src/Projects/components/Proj_Papercut.jsx
+++ src/Projects/components/Proj_Papercut.jsx
@@ -19,14 +19,17 @@ export default function Papercut() {
                 </div>
                 <div className="M">
                     <img src={mongo} alt="mongo" className='mongo'/>
+                    
                 </div>
                 <div className="E">
                     <img src={express} alt="express" className='express'/>
+                    <p className='merntext'> Express </p>
                 </div>
             </div>
             <div className="proj1part2">
                 <div className="R">
                     <img src={react} alt="react" className='react'/>
+                    <p className='merntext'> React </p>
                 </div>
                 <div className="N">
                     <img src={node} alt="node" className='node'/>
@@ -38,10 +41,23 @@ export default function Papercut() {
                 </div>
             </div>
             <div className="proj1description">
-                <h2>
-                    PaperCut
-                </h2>
-                <p> A MERN PROJECT</p>
+                <h2> PAPERCUT </h2>
+                <p className='stack'> 
+                    <span className='mongotext'> MongoDB Atlas </span> | 
+                    <span className="expresstext"> Express </span>  | 
+                    <span className="reacttext"> React </span>  |
+                    <span className="nodetext"> Node </span> |
+                    <span className="verceltext"> Vercel </span>
+                </p>
+                <p className='descriptiontext'> 
+                    &nbsp;&nbsp;&nbsp;&nbsp; PaperCut is a platform where users can discover
+                    and exchange recommendations for books, series, movies, music, and more.
+                </p>
+                <p className='descriptiontext'> 
+                    &nbsp;&nbsp;&nbsp;&nbsp; It is a React website that makes HTTP 
+                    requests to an API I built using Node.js, Express, and MongoDB Atlas.
+                    Then, was deployed it on Vercel.
+                </p>
             </div>
             
         </div>
